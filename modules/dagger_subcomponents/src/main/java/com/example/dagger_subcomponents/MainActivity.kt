353a0bdba74a3970adda98b72e49663f45dcb38f
Youhei Oonuma <oonuma617@gmail.com>
2020-03-25 20:08:07 +0900

    dagger subcomponents

---
+++ modules/dagger_subcomponents/src/main/java/com/example/dagger_subcomponents/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.dagger_subcomponents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dagger_subcomponents.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ modules/dagger_subcomponents/src/main/java/com/example/dagger_subcomponents/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.dagger_subcomponents.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
