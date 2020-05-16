99c067758d9c1baf8402f88e98fac55b72184106
Youhei Oonuma <oonuma617@gmail.com>
2020-05-16 16:45:32 +0900

    add transition

--- modules/transition/src/main/java/com/example/transition/MainActivity.kt
+++ modules/transition/src/main/java/com/example/transition/MainActivity.kt
@@ -1,19 +1,12 @@
-package com.example.animation
+package com.example.transition
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.commitNow
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.commitNow {
-                replace(R.id.main_container, MainFragment())
-            }
-        }
     }
 }
