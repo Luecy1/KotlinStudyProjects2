29c4377b4acb85c448ca3919fa518d76c5a70126
Youhei Oonuma <oonuma617@gmail.com>
2020-05-18 22:53:47 +0900

    add graph

--- ui/myapplication/src/main/java/com/example/myapplication/MainActivity.kt
+++ ui/myapplication/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,44 +3,127 @@ package com.example.myapplication
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.formatter.PercentFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
+import com.github.mikephil.charting.utils.MPPointF
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    private val parties = arrayOf(
+        "Party A", "Party B", "Party C", "Party D", "Party E", "Party F", "Party G", "Party H",
+        "Party I", "Party J", "Party K", "Party L", "Party M", "Party N", "Party O", "Party P",
+        "Party Q", "Party R", "Party S", "Party T", "Party U", "Party V", "Party W", "Party X",
+        "Party Y", "Party Z"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupPieChartView(chart)
+        setupPieChartView()
+        setData()
     }
 
-    private fun setupPieChartView(pieChart: PieChart) {
-        pieChart.setUsePercentValues(true)
-        val legend: Legend = pieChart.legend
+    private fun setupPieChartView() {
+        chart.setUsePercentValues(true);
+        chart.description.isEnabled = false;
+        chart.setExtraOffsets(5f, 10f, 5f, 5f);
 
-        // 円グラフに表示するデータ
-        val values: List<Float> = listOf(40f, 30f, 20f, 10f)
-        val entries: List<PieEntry> = values.map {
-            PieEntry(it)
+        chart.dragDecelerationFrictionCoef = 0.95f
+
+
+        chart.isDrawHoleEnabled = true
+        chart.setHoleColor(Color.WHITE)
+
+        chart.setTransparentCircleColor(Color.WHITE)
+        chart.setTransparentCircleAlpha(110)
+
+        chart.holeRadius = 58f
+        chart.transparentCircleRadius = 61f
+
+
+        chart.rotationAngle = 0f
+        // enable rotation of the chart by touch
+        // enable rotation of the chart by touch
+//        chart.isRotationEnabled = true
+//        chart.isHighlightPerTapEnabled = true
+
+        // chart.setUnit(" €");
+        // chart.setDrawUnitsInChart(true);
+
+        // add a selection listener
+
+        // chart.setUnit(" €");
+        // chart.setDrawUnitsInChart(true);
+
+
+        chart.animateY(1400, Easing.EaseInOutQuad)
+        // chart.spin(2000, 0, 360);
+
+        // chart.spin(2000, 0, 360);
+        val l = chart.legend
+        l.verticalAlignment = Legend.LegendVerticalAlignment.TOP
+        l.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
+        l.orientation = Legend.LegendOrientation.VERTICAL
+        l.setDrawInside(false)
+        l.xEntrySpace = 7f
+        l.yEntrySpace = 0f
+        l.yOffset = 0f
+
+        // entry label styling
+
+        // entry label styling
+        chart.setEntryLabelColor(Color.WHITE)
+        chart.setEntryLabelTextSize(12f)
+    }
+
+    private fun setData() {
+
+        val range = 10f
+
+        val entries: ArrayList<PieEntry> = ArrayList()
+
+        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
+        // the chart.
+        for (i in 0 until 5) {
+            entries.add(
+                PieEntry(
+                    (Math.random() * range + range / 5).toFloat(),
+                    parties[i % parties.size]
+                )
+            )
         }
+        val dataSet = PieDataSet(entries, "Election Results")
+        dataSet.setDrawIcons(false)
+        dataSet.sliceSpace = 3f
+        dataSet.iconsOffset = MPPointF(0f, 40f)
+        dataSet.selectionShift = 5f
 
-        val dataSet = PieDataSet(entries, "チャートのラベル")
-        dataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
-        dataSet.setDrawValues(true)
-        val labels: List<String> = listOf("A", "B", "C", "D")
+        // add a lot of colors
+        val colors: ArrayList<Int> = ArrayList()
+        for (c in ColorTemplate.VORDIPLOM_COLORS) colors.add(c)
+        for (c in ColorTemplate.JOYFUL_COLORS) colors.add(c)
+        for (c in ColorTemplate.COLORFUL_COLORS) colors.add(c)
+        for (c in ColorTemplate.LIBERTY_COLORS) colors.add(c)
+        for (c in ColorTemplate.PASTEL_COLORS) colors.add(c)
+        colors.add(ColorTemplate.getHoloBlue())
+        dataSet.colors = colors
+        //dataSet.setSelectionShift(0f);
+        val data = PieData(dataSet)
+        data.setValueFormatter(PercentFormatter(chart))
+        data.setValueTextSize(11f)
+        data.setValueTextColor(Color.WHITE)
+        chart.data = data
 
-        val pieData = PieData(dataSet)
-        pieData.setValueFormatter(PercentFormatter())
-        pieData.setValueTextSize(12f)
-        pieData.setValueTextColor(Color.WHITE)
-        pieChart.data = pieData
+        // undo all highlights
+        chart.highlightValues(null)
+        chart.invalidate()
     }
 }
